391a14aaca7877b0eee0e19998b7d8ace1a3e72c
lushuaiyuan <lsy_itsports@163.com>
2018-10-16 10:20:13 +0800

    九鼎—日用品出库记录添加项目部和名称

--- app/src/main/java/com/zzti/lsy/ninetingapp/home/parts/LifeGoodsOutRecordActivity.java
+++ app/src/main/java/com/zzti/lsy/ninetingapp/home/parts/LifeGoodsOutRecordActivity.java
@@ -1,10 +1,18 @@
 package com.zzti.lsy.ninetingapp.home.parts;
 
+import android.graphics.DashPathEffect;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 
+import com.bin.david.form.core.SmartTable;
+import com.bin.david.form.core.TableConfig;
+import com.bin.david.form.data.column.Column;
+import com.bin.david.form.data.format.bg.BaseBackgroundFormat;
+import com.bin.david.form.data.style.FontStyle;
+import com.bin.david.form.data.style.LineStyle;
+import com.bin.david.form.data.table.TableData;
 import com.scwang.smartrefresh.layout.SmartRefreshLayout;
 import com.scwang.smartrefresh.layout.api.RefreshLayout;
 import com.scwang.smartrefresh.layout.listener.OnRefreshLoadMoreListener;
@@ -12,10 +20,11 @@ import com.zzti.lsy.ninetingapp.R;
 import com.zzti.lsy.ninetingapp.base.BaseActivity;
 import com.zzti.lsy.ninetingapp.entity.LaobaoDelivery;
 import com.zzti.lsy.ninetingapp.entity.MsgInfo;
+import com.zzti.lsy.ninetingapp.entity.StatisticalList;
 import com.zzti.lsy.ninetingapp.event.C;
-import com.zzti.lsy.ninetingapp.home.adapter.LifeGoodsOutRecordAdapter;
 import com.zzti.lsy.ninetingapp.network.OkHttpManager;
 import com.zzti.lsy.ninetingapp.network.Urls;
+import com.zzti.lsy.ninetingapp.utils.DensityUtils;
 import com.zzti.lsy.ninetingapp.utils.ParseUtils;
 import com.zzti.lsy.ninetingapp.utils.StringUtil;
 import com.zzti.lsy.ninetingapp.utils.UIUtils;
@@ -35,12 +44,11 @@ import butterknife.BindView;
 public class LifeGoodsOutRecordActivity extends BaseActivity {
     @BindView(R.id.smartRefreshLayout)
     SmartRefreshLayout smartRefreshLayout;
-    @BindView(R.id.mRecycleView)
-    RecyclerView mRecycleView;
-    private List<LaobaoDelivery> laobaoDeliveries;
-    private LifeGoodsOutRecordAdapter lifeGoodsOutRecordAdapter;
+    @BindView(R.id.table)
+    SmartTable smartTable;
     private String lbID;
     private int pageIndex = 1;
+
 
     @Override
     public int getContentViewId() {
@@ -53,13 +61,8 @@ public class LifeGoodsOutRecordActivity extends BaseActivity {
         initData();
     }
 
-
     private void initData() {
         lbID = UIUtils.getStr4Intent(this, "lbID");
-        mRecycleView.setLayoutManager(new LinearLayoutManager(this));
-        laobaoDeliveries = new ArrayList<>();
-        lifeGoodsOutRecordAdapter = new LifeGoodsOutRecordAdapter(laobaoDeliveries);
-        mRecycleView.setAdapter(lifeGoodsOutRecordAdapter);
         showDia();
         getRecord();
         smartRefreshLayout.setOnRefreshLoadMoreListener(new OnRefreshLoadMoreListener() {
@@ -72,7 +75,6 @@ public class LifeGoodsOutRecordActivity extends BaseActivity {
             @Override
             public void onRefresh(@NonNull RefreshLayout refreshLayout) {
                 pageIndex = 1;
-                laobaoDeliveries.clear();
                 getRecord();
             }
         });
@@ -86,11 +88,11 @@ public class LifeGoodsOutRecordActivity extends BaseActivity {
         HashMap<String, String> params = new HashMap<>();
         if (!StringUtil.isNullOrEmpty(lbID)) {
             params.put("wherestr", "lbID=" + lbID);
-        }else{
+        } else {
             params.put("wherestr", "");
         }
         params.put("pageIndex", String.valueOf(pageIndex));
-        OkHttpManager.postFormBody(Urls.PARTS_GETLAOBAOOUT, params, mRecycleView, new OkHttpManager.OnResponse<String>() {
+        OkHttpManager.postFormBody(Urls.PARTS_GETLAOBAOOUT, params, smartRefreshLayout, new OkHttpManager.OnResponse<String>() {
             @Override
             public String analyseResult(String result) {
                 return result;
@@ -101,12 +103,14 @@ public class LifeGoodsOutRecordActivity extends BaseActivity {
                 cancelDia();
                 endRefresh(smartRefreshLayout);
                 MsgInfo msgInfo = ParseUtils.parseJson(s, MsgInfo.class);
+                List<LaobaoDelivery> laobaoDeliveries = new ArrayList<>();
                 if (msgInfo.getCode() == 200) {
                     try {
                         JSONArray jsonArray = new JSONArray(msgInfo.getData());
                         if (jsonArray.length() > 0) {
                             for (int i = 0; i < jsonArray.length(); i++) {
                                 LaobaoDelivery laobaoDelivery = ParseUtils.parseJson(jsonArray.getString(i), LaobaoDelivery.class);
+                                laobaoDelivery.setLdDate(laobaoDelivery.getLdDate().split("T")[0]);
                                 laobaoDeliveries.add(laobaoDelivery);
                             }
                         } else {
@@ -123,7 +127,7 @@ public class LifeGoodsOutRecordActivity extends BaseActivity {
                 } else {
                     UIUtils.showT(msgInfo.getMsg());
                 }
-                lifeGoodsOutRecordAdapter.notifyDataSetChanged();
+                setTable(laobaoDeliveries);
             }
 
             @Override
@@ -135,11 +139,48 @@ public class LifeGoodsOutRecordActivity extends BaseActivity {
         });
     }
 
+    //日期  经手人 出库数量   领用人
+    private void setTable(List<LaobaoDelivery> laobaoDeliveries) {
+        //普通列
+        Column<String> column1 = new Column<>("名称", "lbName");
+        column1.setFixed(true);
+        Column<String> column2 = new Column<>("日期", "ldDate");
+        Column<String> column3 = new Column<>("项目部", "projectName");
+        Column<String> column4 = new Column<>("经手人", "useName");
+        Column<String> column5 = new Column<>("出库数量", "ldNumber");
+        Column<String> column6 = new Column<>("领用人", "staffName");
+        if (pageIndex == 1) {
+            //表格数据 datas是需要填充的数据
+            TableData<LaobaoDelivery> tableData = new TableData<>("日用品出库", laobaoDeliveries, column1, column2, column3, column4, column5, column6);
+            //table.setZoom(true,3);是否缩放
+            smartTable.setTableData(tableData);
+        } else {
+            smartTable.addData(laobaoDeliveries, true);
+        }
+    }
+
     private void initView() {
         setTitle("表格查看");
         smartRefreshLayout.setEnableLoadMore(true);
         smartRefreshLayout.setEnableRefresh(true);
         //使上拉加载具有弹性效果：
         smartRefreshLayout.setEnableAutoLoadMore(false);
+
+        TableConfig tableConfig = smartTable.getConfig();
+        tableConfig.setVerticalPadding(DensityUtils.dp2px(8));
+        tableConfig.setShowTableTitle(false);//不显示表格标题
+        tableConfig.setShowXSequence(false);//不显示顶部序号列
+        tableConfig.setShowYSequence(false);//不显示左侧序号列
+        tableConfig.setFixedYSequence(true);//固定左侧
+        tableConfig.setFixedYSequence(true);//固定顶部
+        tableConfig.setColumnTitleBackground(new BaseBackgroundFormat(getResources().getColor(R.color.color_f5f5f5)));//设置列标题背景
+        FontStyle fontStyle = new FontStyle(this, 15, ContextCompat.getColor(this, R.color.color_333333));
+        //设置标题的字体样式
+        LineStyle lineStyle = new LineStyle();
+        lineStyle.setColor(ContextCompat.getColor(this, R.color.color_333333));
+        lineStyle.setEffect(new DashPathEffect(new float[]{5, 5}, 0));
+        tableConfig.setColumnTitleStyle(fontStyle);
+        tableConfig.setColumnTitleVerticalPadding(DensityUtils.dp2px(8));   //设置标题的间距  列标题上下
+        tableConfig.setColumnTitleHorizontalPadding(DensityUtils.dp2px(10));   //设置标题的间距  列标题左右
     }
 }
